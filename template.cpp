6b65e5a3cae88e9fe77d30e7db62ba2d1a1bd2be
hacklu <hacklu.newborn@gmail.com>
2012-12-14 20:48:39 +0800

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+/*
+ * pojxxx  xxxx
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+int compare(const void* a,const void* b)
+{
+	return 0;
+}
+
+//qsort(a,n,sizeof(a[0]),compare);
+int main()
+{
+	int T;
+
+	scanf("%d",&T);
+	while(T--){
+
+	}
+	return 0;
+}
